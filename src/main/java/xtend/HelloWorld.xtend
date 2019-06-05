9cdc6fce18cfad3cc3dc982f2196d52f63e5ea78
Mark <18009838166@163.com>
2019-06-05 19:10:21 +0800

    xtend语言入门

---
+++ src/main/java/xtend/HelloWorld.xtend
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package xtend
+
+/**
+ * @author 18009
+ *
+ */
+class HelloWorld {
+	def static void main(String[] args) {
+		print("helloworld")
+	}
+}
---
+++ src/main/xtend-gen/xtend/HelloWorld.java
@@ -0,0 +1,13 @@
+package xtend;
+
+import org.eclipse.xtext.xbase.lib.InputOutput;
+
+/**
+ * @author 18009
+ */
+@SuppressWarnings("all")
+public class HelloWorld {
+  public static void main(final String[] args) {
+    InputOutput.<String>print("helloworld");
+  }
+}
